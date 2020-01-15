dcaf3d7a00dd3a58d434856bb710d18e4ea7c08d
Michal Gallus <sand3r@interia.eu>
2020-01-15 13:36:53 +0100

    Refactor image loading

--- image_loading.py
+++ image_loading.py
@@ -5,16 +5,20 @@ def load_cv_image(bmp_dir, filename, cv_flag):
     image = cv2.imread(path, cv_flag)
     return image
 
+def get_combined_color_and_alpha(bmp_dir, color_filename, alpha_filename):
+    color_img = load_cv_image(bmp_dir, color_filename, cv2.IMREAD_COLOR)
+    alpha_img = load_cv_image(bmp_dir, alpha_filename, cv2.IMREAD_GRAYSCALE)
+
+    b_channel, g_channel, r_channel = cv2.split(color_img)
+    image = cv2.merge((b_channel, g_channel, r_channel, alpha_img))
+    return image
+
 def merge_color_with_alpha_images(bmp_dir, file_pairs, save_imgs):
     images = []
     for color_filename, alpha_filename in file_pairs:
-        color_img = load_cv_image(bmp_dir, color_filename, cv2.IMREAD_COLOR)
-        alpha_img = load_cv_image(bmp_dir, alpha_filename, cv2.IMREAD_GRAYSCALE)
+        image = get_combined_color_and_alpha(bmp_dir, color_filename, alpha_filename)
+        images.append(image)
 
-        b_channel, g_channel, r_channel = cv2.split(color_img)
-        # alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 50 #creating a dummy alpha channel image.
-        img_BGRA = cv2.merge((b_channel, g_channel, r_channel, alpha_img))
-        images.append(img_BGRA)
         if save_imgs:
             cv2.imwrite(bmp_dir + color_filename[:-len(".bmp")] + ".png", img_BGRA)
     return images

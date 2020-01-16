b4611a1efef274a6b00ac574b90ae4c078a02f86
Michal 'Sand3r' Gallus <sand3r@interia.eu>
2020-01-16 23:46:56 +0100

    Link images to filenames
    Also flip transparency and images themselves vertically

--- image_loading.py
+++ image_loading.py
@@ -10,22 +10,23 @@ def get_combined_color_and_alpha(bmp_dir, color_filename, alpha_filename):
     alpha_img = load_cv_image(bmp_dir, alpha_filename, cv2.IMREAD_GRAYSCALE)
 
     b_channel, g_channel, r_channel = cv2.split(color_img)
-    image = cv2.merge((b_channel, g_channel, r_channel, alpha_img))
+    image = cv2.merge((b_channel, g_channel, r_channel, 255 - alpha_img))
+    image = cv2.flip(image, 0)
     return image
 
 def merge_color_with_alpha_images(bmp_dir, file_pairs, save_imgs):
     images = []
     for color_filename, alpha_filename in file_pairs:
         image = get_combined_color_and_alpha(bmp_dir, color_filename, alpha_filename)
-        images.append(image)
+        images.append((color_filename, image))
 
         if save_imgs:
-            cv2.imwrite(bmp_dir + color_filename[:-len(".bmp")] + ".png", img_BGRA)
+            cv2.imwrite(bmp_dir + color_filename[:-len(".bmp")] + ".png", image)
     return images
 
 def read_png_images(png_dir, filenames):
     images = []
     for filename in filenames:
         img = load_cv_image(png_dir, filename, cv2.IMREAD_UNCHANGED)
-        images.append(img)
+        images.append((filename, img))
     return images
